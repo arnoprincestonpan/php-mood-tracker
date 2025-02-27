6ff14c8eb06e0d843e6f9770583c1a8ff457ad0a
Arno Pan <apan11@my.bcit.ca>
2025-02-27 15:57:15 -0800

    Removed existing default Vite code. Added in packages required: useState, useEffect and axios. Initalize mood String useState and moods useState array. UseEffect() to retrieve moods.db information right away with a func called fetchMoods where axios is used to GET.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mood, setMood] = useState('');
+  const [moods, setMoods] = useState([]);
+
+  // location of api is in back-end folder
+  const backendBaseURL = 'http://localhost:8000/back-end/api.php'
+
+  // GET the moods right when App.jsx loads
+  useEffect(() => {
+    fetchMoods();
+  }, []);
+
+  const fetchMoods = async() => {
+    try {
+      const response = await axios.get(backendBaseURL);
+      setMoods(response.date);
+    } catch (error){
+      console.error('Error fetching moods:', error);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Mood Tracker</h1>
     </>
   )
 }
